bf1c8eee05ad569d843bbbe6d0deef9a9bfee47c
Nurdin <nurdin.ismail@student.moringaschool.com>
2023-07-06 21:37:34 +0300

    Finished adding the sidebar click events That help render the newest books of that genre.

--- src/index.js
+++ src/index.js
@@ -17,11 +17,75 @@ let minReadCount = 2
 
 let fantasy = document.querySelector('#fantasy')
 
+let scifi = document.querySelector('#sci-fi')
+let humor = document.querySelector('#humor')
+let romance = document.querySelector('#romance')
+let crime = document.querySelector('#crime')
+
+
+let bio = document.querySelector('#bio')
+let histor = document.querySelector('#histor')
+let selfhelp= document.querySelector('#selfhelp')
+let Arts = document.querySelector('#Arts')
+
+
+
 fantasy.addEventListener('click', e => {
      handleGenres(e.target.innerText)
 
 
   })
+
+scifi.addEventListener('click', e => {
+    handleGenres(e.target.innerText)
+
+
+ })
+ 
+humor.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+romance.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+
+crime.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+bio.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+histor.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+selfhelp.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+Arts.addEventListener('click', e => {
+  handleGenres(e.target.innerText)
+
+
+})
+
+
 
 
 
@@ -90,11 +154,13 @@ fetch(baseUrlo + endpoint + '?' + params)
 // supriseMe.addEventListener('click',renderSupriseMe(genre))
 }
 
-
+let firstbooksdiv = document.querySelector('#firstbooksdiv')
+let secondbooksdiv = document.querySelector('#secondbooksdiv')
 
 function renderNewBooks(booksArray){
-  let booksSection = document.querySelector('#books-section')
-  booksSection.innerHTML = ` `
+  
+  firstbooksdiv.innerHTML = ` `
+  secondbooksdiv.innerHTML = ` `
   if(booksArray.length % 2 === 0 ){
     let newBooksArray =[...booksArray]
     let newBooksArray2 = [...newBooksArray]
@@ -105,8 +171,68 @@ function renderNewBooks(booksArray){
     console.log(div2)
 
     div1.forEach(book => {
+      cover = getCoverImageUrl(book)
+      let bookdiv = document.createElement('div');
+      firstbooksdiv.appendChild(bookdiv)
+      bookdiv.innerHTML = ` 
+                  <div class="bookdiv">
+                        <div>
+                            <img src="${cover}" alt="" class="img">
+
+
+                        </div>
+                        <div>
+                            <p><span style="font-weight: 700;">Title: </span>${book.title}</p>
+                            <br>
+                            <p> <span style="font-weight: 700;">Author: </span>${book.author_name[0]}</p>
+
+                        </div>
+                  </div>
       
+      `
+
+
+      
+      
+      
+
+
+
+
     })
+
+    div2.forEach(book => {
+      cover = getCoverImageUrl(book)
+      let bookdiv = document.createElement('div');
+      secondbooksdiv.appendChild(bookdiv)
+      bookdiv.innerHTML = ` 
+                  <div class="bookdiv">
+                        <div>
+                            <img src="${cover}" alt="" class="img">
+
+
+                        </div>
+                        <div>
+                            <p><span style="font-weight: 700;">Title: </span>${book.title}</p>
+                            <br>
+                            <p> <span style="font-weight: 700;">Author: </span>${book.author_name[0]}</p>
+
+                        </div>
+                  </div>
+      
+      `
+
+
+      
+      
+      
+
+
+
+
+    })
+
+    
 
 
 
@@ -118,6 +244,70 @@ function renderNewBooks(booksArray){
     let div2 = newBooksArray2.splice(chunk, chunk + newBooksArray2.length)
     console.log(div1)
     console.log(div2)
+
+    div1.forEach(book => {
+      cover = getCoverImageUrl(book)
+      let bookdiv = document.createElement('div');
+      firstbooksdiv.appendChild(bookdiv)
+      bookdiv.innerHTML = ` 
+                  <div class="bookdiv">
+                        <div>
+                            <img src="${cover}" alt="" class="img">
+
+
+                        </div>
+                        <div>
+                            <p><span style="font-weight: 700;">Title: </span>${book.title}</p>
+                            <br>
+                            <p> <span style="font-weight: 700;">Author: </span>${book.author_name[0]}</p>
+
+                        </div>
+                  </div>
+      
+      `
+
+
+      
+      
+      
+
+
+
+
+    })
+
+    div2.forEach(book => {
+      cover = getCoverImageUrl(book)
+      let bookdiv = document.createElement('div');
+      secondbooksdiv.appendChild(bookdiv)
+      bookdiv.innerHTML = ` 
+                  <div class="bookdiv">
+                        <div>
+                            <img src="${cover}" alt="" class="img">
+
+
+                        </div>
+                        <div>
+                            <p><span style="font-weight: 700;">Title: </span>${book.title}</p>
+                            <br>
+                            <p> <span style="font-weight: 700;">Author: </span>${book.author_name[0]}</p>
+
+                        </div>
+                  </div>
+      
+      `
+
+
+      
+      
+      
+
+
+
+
+    })
+
+
   }
   
   
@@ -130,4 +320,8 @@ function renderBestRated(genre){
 
 function renderSupriseMe(genre){
   console.log('hello')
+}
+function getCoverImageUrl(book){
+   const coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+   return coverUrl
 }

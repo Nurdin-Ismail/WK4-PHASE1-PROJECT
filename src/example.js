f8cbdbc8918cec260c260b473d09f1a2d4067d37
Nurdin <nurdin.ismail@student.moringaschool.com>
2023-07-06 19:21:15 +0300

    adding click events and finishing up on the styling

--- src/example.js
+++ src/example.js
@@ -1,70 +1,69 @@
 
 
-const baseUrl = 'http://openlibrary.org';
-const endpoint = '/search.json';
-const subject = 'fantasy'; // Filter books with subject 'Fiction'
-const limit = 50; // Number of books to fetch
-const language = 'eng'
-const minReadCount = 5
 
-const params = new URLSearchParams({
-  subject: subject,
-  limit: limit,
-  sort: '',
-  language: language,
-  first_publish_year: [`2010`]
-});
 
-fetch(baseUrl + endpoint + '?' + params)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Error: ' + response.status);
-    }
-  })
-  .then(data => {
-    // Process the books retrieved
-    data.docs.forEach(book => {
-     
-      const filteredBooks = data.docs.filter(book => book.want_to_read_count > minReadCount);
 
-    // Process the filtered books
-    filteredBooks.forEach(book => {
-      console.log(book)
-      console.log('Title:', book.title);
-      console.log('Read Count:', book.want_to_read_count || 'Unknown')
-      
-      // getCoverImageUrl(book, coverUrl => {
-      //   console.log('Title:', book.title);
-      //   console.log('Author:', book.author_name ? book.author_name.join(', ') : 'Unknown');
-      //   console.log('Read Count:', book.read_count || 'Unknown');
-      //   console.log('Cover Image:', coverUrl);
-      //   console.log('---');
-      // });
-    });
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
 
-// const genre = 'fiction';
-// const rating = 4;
-// const ratingsCount = 1000;
-// const apiKey = 'AIzaSyCxW7IiybgVrpn1z-bYXTu4ai_asOaJehU'; // Replace with your actual API key
-
-// const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject= ${genre}&categories:${genre}&minRating=${rating}&minRatingsCount=${ratingsCount}&orderBy=newest&key=${apiKey}`;
-
-// fetch(apiUrl)
-//   .then(response => response.json())
+// fetch(baseUrlo + endpoint + '?' + params)
+//   .then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error('Error: ' + response.status);
+//     }
+//   })
 //   .then(data => {
-//     // Process the fetched data
-//     const books = data;
-//     console.log(books); // Output the fetched books
+//     // Process the books retrieved
+//     data.docs.forEach(book => {
+     
+//       const filteredBooks = data.docs.filter(book => book.want_to_read_count > minReadCount);
+//        filteredBooks.sort(function(b, a){
+//         var keyA =  a.want_to_read_count ,
+//             keyB =  b.want_to_read_count ;
+//         // Compare the 2 dates
+//         if(keyA < keyB) return -1;
+//         if(keyA > keyB) return 1;
+//         return 0;
+//       })
+//     // Process the filtered books
+//     filteredBooks.forEach(book => {
+      
+//       console.log('Title:', book.title);
+//       console.log(book)
+//       console.log('Read Count:', book.want_to_read_count || 'Unknown')
+      
+//       // getCoverImageUrl(book, coverUrl => {
+//       //   console.log('Title:', book.title);
+//       //   console.log('Author:', book.author_name ? book.author_name.join(', ') : 'Unknown');
+//       //   console.log('Read Count:', book.read_count || 'Unknown');
+//       //   console.log('Cover Image:', coverUrl);
+//       //   console.log('---');
+//       // });
+//     });
+    
+
+//     });
 //   })
 //   .catch(error => {
-//     console.log('An error occurred while fetching the books:', error);
+//     console.log(error);
 //   });
+
+// // const genre = 'fiction';
+// // const rating = 4;
+// // const ratingsCount = 1000;
+// // const apiKey = 'AIzaSyCxW7IiybgVrpn1z-bYXTu4ai_asOaJehU'; // Replace with your actual API key
+
+// // const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject= ${genre}&categories:${genre}&minRating=${rating}&minRatingsCount=${ratingsCount}&orderBy=newest&key=${apiKey}`;
+
+// // fetch(apiUrl)
+// //   .then(response => response.json())
+// //   .then(data => {
+// //     // Process the fetched data
+// //     const books = data;
+// //     console.log(books); // Output the fetched books
+// //   })
+// //   .catch(error => {
+// //     console.log('An error occurred while fetching the books:', error);
+// //   });
 
   
--- src/index.js
+++ src/index.js
@@ -1,22 +1,133 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show");
+let baseUrlo = 'http://openlibrary.org';
+let endpoint = '/search.json';
+; // Filter books with subject 'Fiction'
+let limit = 50; // Number of books to fetch
+let language = 'eng'
+let minReadCount = 2
+
+
+// let params = new URLSearchParams({
+//   subject: subject,
+//   limit: limit,
+//   sort: '',
+//   language: language,
+//   first_publish_year: [`2010`]
+// });
+
+
+let fantasy = document.querySelector('#fantasy')
+
+fantasy.addEventListener('click', e => {
+     handleGenres(e.target.innerText)
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+function handleGenres(genre){
+  let buug =[]
+   let genrenavbar = document.querySelector('#books-seection-navbar')
+  genrenavbar.innerHTML = ` `
+  genrenavbar.innerHTML =` 
+                <nav class="genre-navbar">
+                   <button id="new-releases">New Releases</button>
+                   <button id="best-rated">Best Rated</button>
+                   <button id="suprise-me">Suprise Me!</button>
+                </nav>
+  
+  `
+let subject = `${genre}`
+let genrename =document.querySelector('#genrename')
+ genrename.innerHTML =`${genre}`
+
+let params = new URLSearchParams({
+  subject: subject,
+  limit: 400,
+  
+  language: language,
+  first_publish_year: [`2023`]
+});
+  
+
+fetch(baseUrlo + endpoint + '?' + params)
+.then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Error: ' + res.status);
+      }
+    
+    
+})
+.then(data => {
+  
+  data.docs.forEach(book => {
+    if (book.cover_i ){
+      buug.push(book)
+     
+    }
+     
+  });
+  console.log(buug.length)
+      console.log(buug)
+      renderNewBooks(buug)
+})
+ 
+// let newReleases = document.querySelector('#new-releases')
+// let bestRated = document.querySelector('#best-rated')
+// let supriseMe = document.querySelector('#suprise-me')
+
+// newReleases.addEventListener('click',renderNewBooks(buug))
+// bestRated.addEventListener('click',renderBestRated(genre))
+// supriseMe.addEventListener('click',renderSupriseMe(genre))
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+
+function renderNewBooks(booksArray){
+  let booksSection = document.querySelector('#books-section')
+  booksSection.innerHTML = ` `
+  if(booksArray.length % 2 === 0 ){
+    let newBooksArray =[...booksArray]
+    let newBooksArray2 = [...newBooksArray]
+    let chunk = newBooksArray.length / 2 
+    let div1 = newBooksArray.splice(0, chunk)
+    let div2 = newBooksArray2.splice(chunk, chunk + newBooksArray2.length)
+    console.log(div1)
+    console.log(div2)
+
+    div1.forEach(book => {
+      
+    })
+
+
+
+  }else{
+    let newBooksArray =[...booksArray]
+    let newBooksArray2 = [...newBooksArray]
+    let chunk = newBooksArray.length / 2 + 1
+    let div1 = newBooksArray.splice(0, chunk)
+    let div2 = newBooksArray2.splice(chunk, chunk + newBooksArray2.length)
+    console.log(div1)
+    console.log(div2)
   }
+  
+  
+
+}
+
+function renderBestRated(genre){
+  console.log('hello')
+}
+
+function renderSupriseMe(genre){
+  console.log('hello')
 }
